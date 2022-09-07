9a202538be0ef7a27262425965ddcd4190898203
Angelique C Paulk <APaulk@users.noreply.github.com>
2022-09-06 21:27:10 -0400

    Create OpenEphysLoadSave.m

---
+++ PreprocessingLoading/OpenEphysLoadSave.m
@@ -0,0 +1,27 @@
+%% Example loading of files from OpenEphys 
+%This function requires the functions readNPY and readNPYHeader 
+%from npy-matlab package from kwik-team
+%(https://github.com/kwikteam/npy-matlab)
+
+% and load_open_ephys_binary
+% from: (https://github.com/open-ephys/analysis-tools)
+
+%% OpenEphys loading:
+jsonFile='Y:\IntraOp_Micro\XXXXXXXXXXXXX\XXXXXXXXXXXXX\Record Node 129\experiment1\recording1\structure.oebin';
+addpath(genpath('Y:\IntraOp_Micro\CodeNeuropixels\SpikeCode')) % for the Numpy file read
+
+DAP=load_open_ephys_binary(jsonFile,'continuous',1,'mmap'); %choose 2 for LFP or 1 for AP
+
+DLFP=load_open_ephys_binary(jsonFile,'continuous',2,'mmap'); %choose 2 for LFP or 1 for AP
+
+% save([SaveDirectory,'ExtractedAPData'],'D') %saving the raw data
+
+%% Saving the LFP (OpenEphys):
+
+dataLFP=DLFP.Data.Data(1).mapped;
+fs=2500;
+save([SaveDirectory,'ExtractedLFPData'],'LFP','fs')
+%% Saving the AP band:
+dataAP=DAP.Data.Data(1).mapped;
+fs=30000;
+save([SaveDirectory,'ExtractedAPData'],'APMatrix','fs')
