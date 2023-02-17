ad9774689eabb1b42401499c9f3dc95ce232c7f7
Angelique C Paulk <APaulk@users.noreply.github.com>
2023-02-16 21:58:26 -0500

    Update savingBinaryFilesorMotionRegistraton.m

--- PreprocessingLoading/savingBinaryFilesorMotionRegistraton.m
+++ PreprocessingLoading/savingBinaryFilesorMotionRegistraton.m
@@ -1,38 +1,22 @@
+%% Code for reading 
+
 clf
 clear
-FileNA1={'Pt01','Pt02','Pt03','Pt04','Pt05','Pt06','Pt07','Pt08','Pt09'}
-FileNA2={'NP03_B02_g0_t0',...
-    'NP04_B2_g0_t0',...
-    'NP10_B2_g0_t0',...
-    'NP10_B3_g0_t0',...
-    'NP10_B4_g0_t0',...
-    'NP11_B4_g0_t0',...
-    'NP13_B2_g0_t0',...
-    'NP14_B2_g0_t0',...
-    'NP14_B3_g0_t0',...
-    'NP15_B2_g0_t0'};
 
-Information=readtable(['W:\DeidentifiedNeuropixelsData\DatabasesSpreadsheets\NeuropixelsRecordingInfoAll.xlsx']);
-PtDesig=table2array(Information(:,4));
-ChannelsSaved=table2array(Information(:,15));
-DepthTop=table2array(Information(:,16));
-StartTime=table2array(Information(:,11));
-EndTime=table2array(Information(:,12));
+Ta=readtable(['E:\ExampleNeuropixelsRecordingInformation.xlsx']);
+PtDesig=table2array(Information(:,1));
+ChannelsSaved=table2array(Information(:,16));
+DepthTop=table2array(Information(:,17));
+StartTime=table2array(Information(:,12));
+EndTime=table2array(Information(:,13));
 
-for ns=10:19
-% for ns=1:1
+for ns=1:4
 
-    % subplot(1,10,ns)
-    if ns<10
+
          FileNA=FileNA1{ns};
-        Directory=['W:\DeidentifiedNeuropixelsData\',FileNA1{ns},'\'];
-        load(['W:\DeidentifiedNeuropixelsData\',FileNA1{ns},'\',FileNA1{ns},'_ChannelMap.mat'],'xcoords','ycoords')
-       
-    else
-         FileNA=FileNA2{ns-9};
-        Directory=['W:\DeidentifiedNeuropixelsData\Chang_data\',FileNA2{ns-9},'\'];
-        load(['W:\DeidentifiedNeuropixelsData\Chang_data\',FileNA2{ns-9},'\',FileNA(1:end-6),'_ChannelMap.mat'],'xcoords','ycoords')
-    end
+        Directory=['E:\DataForNatProtocol\',FileNA1{ns},'\'];
+        load(['E:\DataForNatProtocol\',FileNA1{ns},'\',FileNA1{ns},'_ChannelMap.mat'],'xcoords','ycoords')
+      
 
     for wi=1:length(PtDesig)
         if isempty(PtDesig{wi})==0
@@ -60,49 +44,53 @@ for ns=10:19
     dataLFP = fread (fid_source,[ChanTot,Inf],'int16'); %This is a matrix of channel x time, LFP
     fclose(fid_source)
 
-    PerColumn=unique(xcoordsBase);
-    for column=1:4
-        dataLFPPer=dataLFP(xcoordsBase==PerColumn(column),:);
-        resaved_file = [SaveDirectory,'Column',num2str(column),'_',FileNA,'.imec0.lf.bin'];
+        for wi=1:length(PtDesig)
+            if isempty(PtDesig{wi})==0
+                if contains(FileNA,PtDesig{wi})==1
+                    ChanTot=ChannelsSaved(wi);
+                    DepthTopPer=DepthTop(wi);
+                    TimeRange1=StartTime(wi);
+                    TimeRange2=EndTime(wi);
+                    pt3=PtDesig(wi)
+                    %             pause
+                end
+            end
+        end
+        xcoordsBase=xcoords;
+        ycoordsBase=ycoords;
+
+        SaveDirectory=[Directory,'\'];
+        %     mkdir([SaveDirectory])
+
+        %     copyfile([Directory,'\',LFPBinary(1).name],[SaveDirectory,LFPBinary(1).name])
+
+        target_fileLFP = [Directory,'\',LFPBinary(1).name]; %LFP, can replace this with .dat from OpenEphys
+        fid_source = fopen(target_fileLFP,'r');
+        dataLFP = fread (fid_source,[ChanTot,Inf],'int16'); %This is a matrix of channel x time, LFP
+        fclose(fid_source)
+        dataLFPPer=[];
+        dataLFPPer=dataLFP(ycoordsBase<=DepthTopPer,:);
+        xcoords=xcoordsBase(ycoordsBase<=DepthTopPer);
+        ycoords=ycoordsBase(ycoordsBase<=DepthTopPer);
+        ChannelsChosen=find(ycoordsBase<=DepthTopPer);
+        
+        NumberOfChannels=length(ChannelsChosen);
+        resaved_file = [SaveDirectory,'AllinBrain','_',FileNA,'.imec0.lf.bin'];
         fid_target = fopen(resaved_file,'w');
         fwrite(fid_target, dataLFPPer, 'int16' );
         fclose(fid_target)
-        xcoords=xcoordsBase(xcoordsBase==PerColumn(column));
-        ycoords=ycoordsBase(xcoordsBase==PerColumn(column));
-        ChannelsChosen=find(xcoordsBase==PerColumn(column));
-        NumberOfChannels=length(ChannelsChosen);
-        save([SaveDirectory,'Column',num2str(column),'_',FileNA,'_ChannelMap.mat'],'xcoords','ycoords',...
+        save([SaveDirectory,'AllinBrain_',FileNA,'_ChannelMap.mat'],'xcoords','ycoords',...
             'ChannelsChosen','NumberOfChannels',...
             'TimeRange1','TimeRange2','FileNA')
-        column
-    end
 
-    for DepthRange=1:2
-        if DepthRange==1
-            dataLFPPer=dataLFP(ycoordsBase<DepthTopPer/2,:);
-            xcoords=xcoordsBase(ycoordsBase<DepthTopPer/2);
-            ycoords=ycoordsBase(ycoordsBase<DepthTopPer/2);
-            ChannelsChosen=find(ycoordsBase<DepthTopPer/2);
-            NumberOfChannels=length(ChannelsChosen);
-        elseif DepthRange==2
-            dataLFPPer=dataLFP(ycoordsBase>=DepthTopPer/2,:);
-            xcoords=xcoordsBase(ycoordsBase>=DepthTopPer/2);
-            ycoords=ycoordsBase(ycoordsBase>=DepthTopPer/2);
-            ChannelsChosen=find(ycoordsBase>=DepthTopPer/2);
-            NumberOfChannels=length(ChannelsChosen);
-        end
-        resaved_file = [SaveDirectory,'DepthRange',num2str(DepthRange),'_',FileNA,'.imec0.lf.bin'];
-        fid_target = fopen(resaved_file,'w');
-        fwrite(fid_target, dataLFPPer, 'int16' );
-        fclose(fid_target)
+        save([SaveDirectory,'All_',FileNA,'_ChannelMap.mat'],'xcoords','ycoords',...
+            'ChannelsChosen','NumberOfChannels',...
+            'TimeRange1','TimeRange2','FileNA','ChanTot')
 
-        save([SaveDirectory,'DepthRange',num2str(DepthRange),'_',FileNA,'_ChannelMap.mat'],'xcoords','ycoords',...
-            'ChannelsChosen',...
-            'NumberOfChannels',...
-            'TimeRange1','TimeRange2','FileNA')
-        DepthRange
-    end
+        clear xcoords ycoords dataLFP dataLFPPer xcoordsbase ycoordsbase
+        
 
+% Uncomment to plot the data
     %     scatter(xcoords,...
     %         ycoords,48,"black",'marker','s')
     %     hold on
