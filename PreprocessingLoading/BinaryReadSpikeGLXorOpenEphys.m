3cceec75d72040fdcd868d3588d35d287489c069
Angelique C Paulk <APaulk@users.noreply.github.com>
2023-02-16 21:28:52 -0500

    Update BinaryReadSpikeGLXorOpenEphys.m

--- PreprocessingLoading/BinaryReadSpikeGLXorOpenEphys.m
+++ PreprocessingLoading/BinaryReadSpikeGLXorOpenEphys.m
@@ -1,12 +1,12 @@
 %% SpikeGLX or OpenEphys loading (simple binary read):
 
-MainDir='R:\Dropbox (Partners HealthCare)\MotionCorrectionWithNeuropixels\data\NeuropixelsHumanData\';
-target_fileAP = [MainDir,'Pt04\raw\Pt04.imec0.ap.bin']; %AP, can replace this with .dat from OpenEphys
+MainDir='E:\DataForNatProtocol\';
+target_fileAP = [MainDir,'Pt01\raw\Pt01.imec0.ap.bin']; %AP, can replace this with .dat from OpenEphys
 fid_source = fopen(target_file,'r');
 dataAP = fread (fid_source,[385,Inf],'int16'); %This is a matrix of channel x time, AP 
 fclose(fid_source)
 
-target_fileLFP = [MainDir,'Pt04\raw\Pt04.imec0.lf.bin']; %LFP, can replace this with .dat from OpenEphys
+target_fileLFP = [MainDir,'Pt01\raw\Pt01.imec0.lf.bin']; %LFP, can replace this with .dat from OpenEphys
 fid_source = fopen(target_file,'r');
 dataLFP = fread (fid_source,[385,Inf],'int16'); %This is a matrix of channel x time, LFP 
 fclose(fid_source)
