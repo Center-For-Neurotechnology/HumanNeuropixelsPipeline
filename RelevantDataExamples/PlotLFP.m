2d734355f00730959e1202a557691797227e95ed
Angelique C Paulk <APaulk@users.noreply.github.com>
2023-03-01 21:24:48 -0500

    Update PlotLFP.m

--- RelevantDataExamples/PlotLFP.m
+++ RelevantDataExamples/PlotLFP.m
@@ -1,11 +1,14 @@
 %% Code to quickly plot the data to identify what channels are outside the brain versus inside
 % and to identify time ranges for further processing and motion tracking. 
 
-MainDir='E:\DataForNatProtocol\Pt01\';
-target_fileLFP = [MainDir,'rawPt01.imec0.lf.bin']; %LFP, can replace this with .dat from OpenEphys
+
+Pt=01;
+
+MainDir=['E:\DataForNatProtocol\Pt' num2str(Pt) '\'];
+target_fileLFP = [MainDir,'rawPt' num2str(Pt) '.imec0.lf.bin']; %LFP, can replace this with .dat from OpenEphys
 fid_source = fopen(target_fileLFP,'r');
 dataLFP = fread (fid_source,[385,Inf],'int16'); %This is a matrix of channel x time, LFP
-load('E:\DataForNatProtocol\Pt01\Pt01_ChannelMap.mat')
+load([MainDir '\Pt' num2str(Pt) '_ChannelMap.mat'])
  
 %% Example code to plot the LFP data to identify the recording in the brain
 TIME=(1:size(dataLFP,2))/2500;
